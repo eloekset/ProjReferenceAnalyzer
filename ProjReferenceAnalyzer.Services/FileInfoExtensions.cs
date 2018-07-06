b6d150e24b9000e3b66714b1dda06ea9479d62b7
Eivind Gussiås Løkseth <eloekset@msn.com>
2018-07-06 23:26:50 +0200

    Add lookup of ProjectReferences.

---
+++ ProjReferenceAnalyzer.Services/FileInfoExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace ProjReferenceAnalyzer.Services
+{
+    public static class FileInfoExtensions
+    {
+        public static string AbsolutePath(this FileInfo fileInfo)
+        {
+            string absolutePath = fileInfo.FullName;
+
+            while(absolutePath.Contains(".." + Path.DirectorySeparatorChar))
+            {
+                int indexOfUpOneLevel = absolutePath.IndexOf(".." + Path.DirectorySeparatorChar);
+                int indexOfPreviousDirectory = absolutePath.IndexOf(Path.DirectorySeparatorChar, indexOfUpOneLevel - 2);
+                absolutePath = absolutePath.Substring(0, indexOfPreviousDirectory) + absolutePath.Substring(indexOfUpOneLevel + 2);
+            }
+
+            return absolutePath;
+        }
+    }
+}
