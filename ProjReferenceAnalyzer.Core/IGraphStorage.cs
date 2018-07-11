4db753ccbd31d7cd1e19151651c448b26cd72497
Eivind Gussiås Løkseth <eloekset@msn.com>
2018-07-11 20:33:10 +0200

    Export content to JSON

---
+++ ProjReferenceAnalyzer.Core/IGraphStorage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ProjReferenceAnalyzer.Core
+{
+    public interface IGraphStorage
+    {
+        IGraphSerializationFormat SerializationFormat { get; set; }
+        void Store(string graphIdentifier, IEnumerable<SolutionInfo> solutions);
+        IEnumerable<SolutionInfo> Load(string graphIdentifier);
+    }
+}
---
+++ ProjReferenceAnalyzer.Storage.LocalFileSystem/FileStorage.cs
@@ -0,0 +1,23 @@
+﻿using ProjReferenceAnalyzer.Core;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ProjReferenceAnalyzer.Storage
+{
+    public class LocalFileSystemStorage : IGraphStorage
+    {
+        public IGraphSerializationFormat SerializationFormat { get; set; }
+
+        public IEnumerable<SolutionInfo> Load(string graphIdentifier)
+        {
+            string graphData = File.ReadAllText(graphIdentifier);
+            return SerializationFormat.DeserializeGraph(graphData);
+        }
+
+        public void Store(string graphIdentifier, IEnumerable<SolutionInfo> solutions)
+        {
+            string graphData = SerializationFormat.SerializeGraph(solutions);
+            File.WriteAllText(graphIdentifier, graphData);
+        }
+    }
+}
