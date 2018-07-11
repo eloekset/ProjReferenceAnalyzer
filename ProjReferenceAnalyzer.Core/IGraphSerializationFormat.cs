cda3bbad2326095c2ff06617d81304431aa67c6b
Eivind Gussiås Løkseth <eloekset@msn.com>
2018-07-11 20:33:10 +0200

    Export content to JSON

---
+++ ProjReferenceAnalyzer.Core/IGraphSerializationFormat.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ProjReferenceAnalyzer.Core
+{
+    public interface IGraphSerializationFormat
+    {
+        string SerializeGraph(IEnumerable<SolutionInfo> solutions);
+        IEnumerable<SolutionInfo> DeserializeGraph(string graphData);
+    }
+}
