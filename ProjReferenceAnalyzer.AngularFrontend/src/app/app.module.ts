d2e4a33999374974995b364e75589f7f0d5e6731
Eivind Gussiås Løkseth <eloekset@msn.com>
2018-08-11 20:17:41 +0200

    Add ng2-vis with network sample component.

--- ProjReferenceAnalyzer.AngularFrontend/src/app/app.module.ts
+++ ProjReferenceAnalyzer.AngularFrontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { VisModule } from 'ng2-vis';
 
 import { AppComponent } from './app.component';
+import { VisnetworkComponent } from './visnetwork/visnetwork.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisnetworkComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    VisModule
   ],
   providers: [],
   bootstrap: [AppComponent]
